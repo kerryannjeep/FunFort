c396d1992df8ab6f2fa383606a856a2fa1f3b5ab
DJ <umdjm@hotmail.com>
2013-11-16 19:37:29 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FunFort::Application.config.secret_token = '050db5a082381bba1ea25500e46f70efa9e2f667d1c67037cbf20d540a7832dd629cf2957954b3905f0c8f7933170b06d4885f36b6fa3a1395989605d4d15ff8'
